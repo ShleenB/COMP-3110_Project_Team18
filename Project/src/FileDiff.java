4e8af458a40a5ce73c37cd42d4b4a5fe114cca28
ShleenB <colbrown28@gmail.com>
2025-11-26 17:40:09 -0500

    (main) Initial Commit: Redid README and added file structure for project [CollinBrown-110140191]

---
+++ Project/src/FileDiff.java
@@ -0,0 +1,24 @@
+
+/**
+ * FileDiff.java
+ * 
+ * <p>
+ * The FileDiff class uses all of our other classes and methods to find the difference between two different
+ * versions of a file or two different files. This includes line additons, changes, and removals.
+ * 
+ * </p>
+ * 
+ * @author      Collin Brown; Ahmad Arain; Hashir Khan; Yousef Al-Wahami; Jacob Dam
+ * 
+ */
+public class FileDiff{
+
+    public static void main(String args[]){
+        
+    }
+
+    public FileDiff(String filepath1,String filepath2){
+
+    }
+
+}
