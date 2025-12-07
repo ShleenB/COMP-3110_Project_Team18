b69bb1d4fbafb1d45fb69602591936b3ca843024
Hashir Khan <khan4l1@uwindsor.ca>
2025-12-07 18:37:09 -0500

    (master) Added unix_diff algorithm to main.py. Note: algorithm is untested at the moment. Also added a few smaller changes like TODO stubs and function headings. IMPORTANT: Also added professor's provided test case folder and the group's (empty as of right now) folder for test cases.[Hashir Khan-110134972]

--- main.py
+++ main.py
@@ -30,7 +30,10 @@ class LHDiff:
 
         #Mapping lists for which old lines will match the new lines
         self.map_old = {}
-        self.map_new = {}
+        #The list of new indices is a set so that we don't match to multiple old lines
+        self.map_new = set()    #Changed to set
+
+
 
     
     #(Step 1)
@@ -38,18 +41,62 @@ class LHDiff:
     def process_line(self, line):
         #strip the line and make it lowercase
         line = line.strip().lower()
-        #Using regex to find all tokens in our line list
-        #This will also get rid of special characters
+        #Using regex to find all tokens in our line list 
         tokens = re.findall(r'\w+', line)
-        #return the string of joined, processed lines
+        #return the string of our joined and processed line
         return " ".join(tokens)
-
     
+    #(Step 2)
+    #Unix Diff Algorithm - get unchanged lines with longest common subsequence
+    #We run this on the un-processed lines to not skew our line change mapping
+    def unix_diff(self):
+        #length of old and new files
+        old_len = len(self.old_lines)
+        new_len = len(self.new_lines)
+
+        #Using sets (which don't have duplicates)
+        #This set tracked which lines we've already matched, to prevent duplication
+        old_set = set()
+
+        #Map all of the exact matches
+        for i in range(old_len):
+            for j in range(new_len):
+                #Ensure we haven't already matched them
+                if i not in old_set and j not in self.map_new:
+                    #If the lines are identical (ie. all of their letters match)
+                    if self.old_lines[i] == self.new_lines[j]:
+                        #Saving line numbers in our mapping
+                        self.map_old = j
+                        self.map_new.add(j)
+                        #
+                        old_set.add(i)
+                        break
+                        #This catches all of the unchanged lines in the LCS
+
+
+
+        
+
+    #(Step 3)
+    # Generate Candidate List
+    #def levenshtein_distance():
+        #TODO
+    #def cosine_similarity():
+        #TODO
+
+    #(Step 3)
+    #def get_
+
     #(unfinished)
     # run: runs the previously defined functions
     # This is where all of the steps culminate (For LHDiff)
-    #def run(self):
+    def run(self):
         
+
+        #Check for unchanged lines
+        self.unix_diff()
+
+
         
 
 
@@ -101,7 +148,7 @@ def compare_files(folder, file1, file2):
     new_file = get_file(file_path2)
 
     #TODO
-    #Use LHdiff
+    #Use LHdiff (needs implementation after LHDiff is completed)
 
     #Generate XML output
 
@@ -128,3 +175,4 @@ if __name__ == "__main__":
     else:
         print("Error: Uneven indices in file list")
         sys.exit(1)
+        
